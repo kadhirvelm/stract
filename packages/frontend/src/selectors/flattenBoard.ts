6b3f7a16ff4a7ce6ea6c63a536a91b8d4e34b5cf
Kadhir Mani <kadhirvelm@gmail.com>
2020-05-18 22:48:54 -0700

    Add comment explaining why

--- packages/frontend/src/selectors/flattenBoard.ts
+++ packages/frontend/src/selectors/flattenBoard.ts
@@ -3,6 +3,11 @@ import { IGameTile } from "@stract/api";
 import { flatten } from "lodash-es";
 import { IStoreState } from "../store";
 
+/**
+ * In order to get the CSS transitions to work reliably, we need to paint them at the same time, not row by row, and
+ * we need to paint the tiles with tokens on them first to ensure tokens moving south work. I'm not entirely certain why
+ * the sort is what fixes that, but it does?
+ */
 export const flattenBoard = createSelector(
     (state: IStoreState) => state.game.gameBoard?.board,
     (board: IGameTile[][] | undefined) => {
