b787a5e629e0a74607e169a2296fe6008ec65327
Kadhir Mani <kadhirvelm@gmail.com>
2020-05-24 23:37:54 -0700

    undo

--- packages/backend/src/game/stractPlayer.ts
+++ packages/backend/src/game/stractPlayer.ts
@@ -45,16 +45,16 @@ export class StractPlayer implements IStractPlayer {
             return;
         }
 
-        // if (
-        //     !IGameState.isInPlay(this.game.currentGameState.state) &&
-        //     !IGameState.isRequestPause(this.game.currentGameState.state)
-        // ) {
-        //     this.toClient.onMessage({
-        //         message: "Unfortunately the game is playing right now. We can't accept your action.",
-        //         intent: "warning",
-        //     });
-        //     return;
-        // }
+        if (
+            !IGameState.isInPlay(this.game.currentGameState.state) &&
+            !IGameState.isRequestPause(this.game.currentGameState.state)
+        ) {
+            this.toClient.onMessage({
+                message: "Unfortunately the game is playing right now. We can't accept your action.",
+                intent: "warning",
+            });
+            return;
+        }
 
         const team = getTeamKeyFromRid(this.team, this.game.currentGameState.teams);
         if (
